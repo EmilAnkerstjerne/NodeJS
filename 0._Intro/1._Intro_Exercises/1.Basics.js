851b940370de20926a1fa300d8da38aad56f7b85
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-08-31 15:06:41 +0200

    Added extra solutions for exercise 1.Basics.js and added 2.Strings.js

--- 0._Intro/1._Intro_Exercises/1.Basics.js
+++ 0._Intro/1._Intro_Exercises/1.Basics.js
@@ -10,6 +10,7 @@ const lastName = "Latif";
 // My first name is Anders and my last name is Latif
 
 console.log("My first name is", firstName, "and my last name is", lastName);
+console.log(`My first name is ${firstName} and my last name is ${lastName}`);
 
 // --------------------------------------
 // Exercise 2 - Numbers and Strings
@@ -22,5 +23,8 @@ const number = 1;
 // You cannot touch line 1 or 2
 
 console.log(parseInt(year) + number);
+console.log(Number(year) + number);
+console.log(+year + number);
+console.log(number + +year);
 
 // --------------------------------------
