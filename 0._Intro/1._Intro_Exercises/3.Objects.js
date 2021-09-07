c1717da1157a0306cc4bedf06f698a01b143585c
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-09-07 02:11:51 +0200

    Completed 3.Objects and 4.Arrays

--- 0._Intro/1._Intro_Exercises/3.Objects.js
+++ 0._Intro/1._Intro_Exercises/3.Objects.js
@@ -7,10 +7,18 @@ const myObj = {"message": "Hello, earthling! I bring peace."};
 
 // Log the message 
 
+console.log(myObj.message);
+
 // --------------------------------------
 // Exercise 2 - Defining an object. 
 
 // Create an object that has your name and age. 
+
+const me = {
+    "firstName": 'Emil',
+    "lastName": 'Ankerstjerne',
+    "age": 23
+}
 
 
 // --------------------------------------
@@ -20,6 +28,8 @@ const stackOverflow = {};
 
 // make a rule called isAllowed and let the value be true
 
+stackOverflow.isAllowed = true;
+
 // --------------------------------------
 // Exercise 4 - Remove a property 
 
@@ -27,6 +37,7 @@ const thisSong = {"description": "The best song in the world."}
 
 // remove the property "description" and add a property called "about" that should say "Just a tribute." 
 
+delete thisSong.description;
 
 // --------------------------------------
 
--- 0._Intro/1._Intro_Exercises/4.Arrays.js
+++ 0._Intro/1._Intro_Exercises/4.Arrays.js
@@ -6,6 +6,7 @@
 const letters = ["a","b","c"];
 // show b in the console 
 
+console.log(letters[1]);
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -14,6 +15,7 @@ const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
 
+friends.push("Brian", "Bob", "Maria");
 
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
@@ -23,6 +25,7 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
 
+console.log(significantMathNumbers.indexOf(1729));
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -32,6 +35,7 @@ const diet = ["tomato", "cucumber", "rocket"];
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
 
 
+diet.splice(diet.indexOf("cucumber")+1, 0, "hamburger", "soda", "pizza");
 
 
 // --------------------------------------
@@ -41,7 +45,7 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
 
-
+diet.pop();
 
 
 // --------------------------------------
@@ -49,7 +53,7 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
+const dinnerTray = diet.slice();
 
 // --------------------------------------
 // Exercise 7 - For loop
@@ -58,7 +62,9 @@ const lettersExpanded = ["a","b","c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
 
-
+for(var i = 1; i < lettersExpanded.length; i+=2){
+    console.log(lettersExpanded[i]);
+}
 
 // --------------------------------------
 // Exercise 8 - For loop and if statement
@@ -69,6 +75,16 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
+
+for(var i = 0; i < numbers.length; i++){
+    if(numbers[i] > 6 || numbers[i]<0){
+        console.log(numbers[i]);
+    }else{
+        discardedNumbers.push(numbers[i]);
+    }
+}
+
+console.log(discardedNumbers);
 
 // --------------------------------------
 
