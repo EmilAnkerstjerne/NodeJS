def919583e14b3306c9890285b1314b24883abc1
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-09-21 14:19:24 +0200

    moved some code around and commented out some code

--- Dank_Memes/app.js
+++ Dank_Memes/app.js
@@ -1,22 +1,22 @@
 const express = require("express");
 const app = express();
 
-const dankmemes = require("./dankmemes.json");
+// const dankmemes = require("./dankmemes.json");
 
-// let dankmemes = [
-//     {
-//         navn: "Rick Astley",
-//         id: 1
-//     },
-//     {
-//         navn: "Stonks",
-//         id: 2
-//     },
-//     {
-//         navn: "Mike Warzowski",
-//         id: 3
-//     },
-// ]
+let dankmemes = [
+    {
+        navn: "Rick Astley",
+        id: 1
+    },
+    {
+        navn: "Stonks",
+        id: 2
+    },
+    {
+        navn: "Mike Warzowski",
+        id: 3
+    },
+]
 console.log(dankmemes.dankmemes[0]);
 
 app.get("/dankmemes", (req, res) => {
@@ -24,14 +24,15 @@ app.get("/dankmemes", (req, res) => {
 })
 
 app.get("/dankmemes/:id", (req, res) => {
-    // res.send(dankmemes.find(dankmeme => dankmeme.id === parseInt(req.params.id)));
+    res.send(dankmemes.find(dankmeme => dankmeme.id === parseInt(req.params.id)));
 
-    for (let i = 0; i < dankmemes.dankmemes.length; i++){
-        if(dankmemes.dankmemes[i].id === parseInt(req.params.id)){
-            return res.send(dankmemes.dankmemes[i]);
-        }
-    }
-    res.send("fejl")
+    //---------- For JSON file ---------
+    // for (let i = 0; i < dankmemes.dankmemes.length; i++){
+    //     if(dankmemes.dankmemes[i].id === parseInt(req.params.id)){
+    //         return res.send(dankmemes.dankmemes[i]);
+    //     }
+    // }
+    // res.send("fejl")
 });
 
 
