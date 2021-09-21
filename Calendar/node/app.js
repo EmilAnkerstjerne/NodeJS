bbfe5c92a8cceff1e834ff99d674370bf0493b47
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-09-21 14:10:13 +0200

    removed unused variable

--- Calendar/node/app.js
+++ Calendar/node/app.js
@@ -4,24 +4,21 @@ const app = express();
 
 app.use(cors())
 
-let day;
-
-
 
 app.get("/time", (req, res) => {
-    let date = new Date();
-    res.send({time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`})
+    const date = new Date();
+    res.send({ time: `${date.getHours()}:${date.getMinutes()}:${date.getSeconds()}`})
     
 })
 
 app.get("/day", (req, res) => {
-    let date = new Date();
-    res.send({day: new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)});
+    const date = new Date();
+    res.send({ day: new Intl.DateTimeFormat('en-US', {weekday: 'long'}).format(date)});
 })
 
 app.get("/month", (req, res) => {
-    let date = new Date();
-    res.send({month: new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date)});
+    const date = new Date();
+    res.send({ month: new Intl.DateTimeFormat('en-US', {month: 'long'}).format(date)});
 })
 
 
