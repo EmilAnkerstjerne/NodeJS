3a33e6d300a4db0e5717e6a4713ed4b9b97d16dd
Emil Ankerstjerne <emil.ankerstjerne@gmail.com>
2021-11-16 00:53:35 +0100

    Added email functionality

--- 5._Nodefolio/routers/contact.js
+++ 5._Nodefolio/routers/contact.js
@@ -1,7 +1,31 @@
 const router = require("express").Router();
+const nodemailer = require("nodemailer");
 
-router.post("/api/contact", (req, res) => {
-    console.log(req.body);
+router.post("/api/contact", async (req, res) => {
+    let transporter = nodemailer.createTransport({
+        host: "smtp.gmail.com",
+        port: 587,
+        secure: false, // true for 465, false for other ports
+        auth: {
+          user: "emilankerstjerne.portfolio@gmail.com",
+          pass: "emil1234!"
+        },
+    });
+
+    let info = await transporter.sendMail({
+        from: '"Emils portfolio" <emilankerstjerne.portfolio@gmail.com>', // sender address
+        to: "emil.ankerstjerne@gmail.com", // list of receivers
+        subject: `New Message from ${req.body.nameInput}, portfolio`, // Subject line
+        text: 
+        `
+        Name: ${req.body.nameInput}\n
+        Email: ${req.body.emailInput}\n
+        Phone No.: ${req.body.phoneInput}\n
+        Message:\n
+        ${req.body.messageInput}`
+      });
+
+
     res.redirect("/contact");
 });
 
